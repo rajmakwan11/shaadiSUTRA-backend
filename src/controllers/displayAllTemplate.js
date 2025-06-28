a56c23b41f53bb505495872434190eec92c71506
Raj Makwana <145688339+rajmakwan11@users.noreply.github.com>
2025-06-29 01:19:46 +0530

    Update displayAllTemplate.js

--- src/controllers/displayAllTemplate.js
+++ src/controllers/displayAllTemplate.js
@@ -5,14 +5,11 @@ const displayAllTemplate = async (req, res) => {
     const getTemplates = await Templates.find({});
 
     const templatesWithFullUrl = getTemplates.map(template => {
-      let imageUrl = template.image;
-
-      if (!/^https?:\/\//i.test(imageUrl)) {
-        // If it doesn't already start with http:// or https://
-        imageUrl = `${process.env.BACKEND_URL}${imageUrl}`;
-      }
-
-      return { ...template._doc, image: imageUrl };
+      const newImage = template.image.replace(
+        "http://localhost:3000",
+        process.env.BACKEND_URL
+      );
+      return { ...template._doc, image: newImage };
     });
 
     res.send(templatesWithFullUrl);
@@ -23,3 +20,4 @@ const displayAllTemplate = async (req, res) => {
 };
 
 module.exports = displayAllTemplate;
+    
